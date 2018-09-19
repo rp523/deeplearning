c8205c7d76ff494451ba1ce75896aafd53987bd4
Yusuke Goto <isgsktyktt@gmail.com>
2018-09-20 00:02:14 +0900

    get_file_listに色々オプション追加

--- common/fileio.py
+++ common/fileio.py
@@ -13,7 +13,11 @@ def get_ext(file_path):
 def get_file_name(file_path):
     return file_path[file_path.rfind(os.sep) + 1:]
 
-def get_file_list(tgt_dir, find_txt = None, tgt_ext = None, recursive = True):
+def get_file_list(tgt_dir,
+                  name_txt = None,
+                  include_path_txt = None,
+                  exclude_path_txt = None,
+                  tgt_ext = None, recursive = True):
     # 着目しているディレクトリ以下のファイルを全取得する
     ret = []
     for item_name in os.listdir(tgt_dir):
@@ -22,15 +26,23 @@ def get_file_list(tgt_dir, find_txt = None, tgt_ext = None, recursive = True):
             if tgt_ext is not None:
                 if get_ext(item_path) != tgt_ext:
                     continue
-            if not find_txt is None:
+            if not name_txt is None:
                 file_name = get_file_name(item_path)
-                if file_name.find(find_txt) < 0:
+                if file_name.find(name_txt) < 0:
+                    continue
+            if not include_path_txt is None:
+                if item_path.find(include_path_txt) < 0:
+                    continue
+            if not exclude_path_txt is None:
+                if item_path.find(exclude_path_txt) >= 0:
                     continue
             ret.append(item_path)
         elif os.path.isdir(item_path):
             if recursive:
                 ret = ret + get_file_list(tgt_dir = item_path,
-                                          find_txt = find_txt,
+                                          name_txt = name_txt,
+                                          include_path_txt = include_path_txt,
+                                          exclude_path_txt = exclude_path_txt,
                                           tgt_ext = tgt_ext,
                                           recursive = recursive)
     return ret
