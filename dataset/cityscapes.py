1178e1a861cbbc6ae90fba77b21e9c888d0c97b6
Yusuke Goto <me@example.com>
2018-09-19 21:31:44 +0900

    最初に全データのパスリストを取得する。RGB画像の取得に対応。

--- dataset/cityscapes.py
+++ dataset/cityscapes.py
@@ -3,6 +3,7 @@ import sys, os, shutil
 import numpy as np
 import json
 from PIL import Image, ImageDraw
+import re
 
 import storage
 from common import fileio
@@ -11,23 +12,41 @@ from dataset_base import label_dict
 class CityScapes:
     def __init__(self):
         s = storage.Storage()
-        self.__data_path = s.dataset_path("cityscapes")
-        self.__coarse_json = {}
+        data_path = s.dataset_path("cityscapes")
+        self.__img_path_list, self.__json_path_list = self.__update_list(data_path)
+        
+    def __update_list(self, data_path):
+        json_path_list = fileio.get_file_list(tgt_dir = data_path,
+                                              tgt_ext = ".json")
+        img_path_list_  = fileio.get_file_list(tgt_dir = data_path,
+                                              tgt_ext = ".png",
+                                              find_txt = "leftImg8bit.png")
+        json_key_list = []
+        for json_path in json_path_list:
+            json_name = fileio.get_file_name(json_path)
+            json_key_list.append(re.search("\D+\d+_\d+", json_name).group())
+        img_key_list = []
+        for img_path in img_path_list_:
+            img_name = fileio.get_file_name(img_path)
+            img_key_list.append(re.search("\D+\d+_\d+", img_name).group())
+        img_path_list = []
+        for json_key in json_key_list:
+            idx = img_key_list.index(json_key)
+            new_path = img_path_list_[idx]
+            img_path_list.append(new_path)
+
+        return img_path_list, json_path_list
     
-    def get_coarse_label(self, label_type, index = None):
-        tgt_dir = os.path.join(self.__data_path, "gtCoarse", "gtCoarse")
-        assert(label_type == "train" or \
-               label_type == "train_extra" or\
-               label_type == "val")
-        if not label_type in self.__coarse_json.keys():
-            tgt_dir = os.path.join(tgt_dir, label_type)
-            assert(os.path.exists(tgt_dir))
-            json_path_list = fileio.get_file_list(tgt_dir = tgt_dir,
-                                                  tgt_ext = ".json")
-            self.__coarse_json[type] = json_path_list
+    def get_one_data(self, index = None):
         if index is None:
-            index = np.random.randint(len(self.__coarse_json[type]))
-        json_path = self.__coarse_json[type][index]
+            index = np.random.randint(len(self.__json_path_list))
+        
+        img_path = self.__img_path_list[index]
+        print(img_path)
+        rgb_img = Image.open(img_path)
+        rgb_map = np.asarray(rgb_img)
+        
+        json_path = self.__json_path_list[index]
         info = json.load(open(json_path))
         h = info["imgHeight"]
         w = info["imgWidth"]
@@ -37,6 +56,7 @@ class CityScapes:
         labels = np.empty(0).astype(np.int)
         rects = np.empty((0, 4)).astype(np.float)
         pixel_img = Image.fromarray(np.zeros((h, w)).astype(np.uint8))
+        pixel_label_map = np.asarray(pixel_img)
         draw = ImageDraw.Draw(pixel_img)
         
         for obj in obj_list:
@@ -63,11 +83,31 @@ class CityScapes:
                 x1 = min(x1, w - 1)
                 y1 = min(y1, h - 1)
                 rects = np.append(rects, np.array([x0 / w, y0 / h, x1 / w, y1 / h]).reshape(1, 4), axis = 0)
-        #pixel_img.resize((w//4,h//4)).show();exit()
-        pixel_map = np.asarray(pixel_img)
-        return labels, rects, pixel_map
+        if 1: #debug view
+            
+            draw = ImageDraw.Draw(pixel_img)
+            dw, dh = pixel_img.size
+            for i in range(rects.shape[0]):
+                rect = rects[i]
+                x0 = int(rect[0] * dw)
+                y0 = int(rect[1] * dh)
+                x1 = int(rect[2] * dw)
+                y1 = int(rect[3] * dh)
+                draw.rectangle([x0, y0, x1, y1], outline = 255)
+                for k, v in label_dict.items():
+                    if v == labels[i]:
+                        draw.text([max(0, min(x0, dw - 1)),
+                                   max(0, min(y0, dw - 1))],
+                                  k,
+                                  fill = 255)
+                        break
+            pixel_img.resize((pixel_img.size[0]//2,pixel_img.size[1]//2)).show()
+            rgb_img.resize((rgb_img.size[0]//2,rgb_img.size[1]//2)).show()
+            exit()
+
+        return rgb_map, labels, rects, pixel_label_map
         
 if __name__ == "__main__":
     c = CityScapes()
-    print(c.get_coarse_label("val"))
+    print(c.get_one_data())
     
