3ba2c8d3342860cd48344f25bee75ae1173a47d4
Yusuke Goto <isgsktyktt@gmail.com>
2018-11-06 06:50:13 +0900

    動くようなおす

--- trainer/mnist_test.py
+++ trainer/mnist_test.py
@@ -16,13 +16,9 @@ def mnist_trial():
     network = ImageNetwork(image_h = 28,
                            image_w = 28,
                            image_ch = 1)
-    network.add_conv(ImageNetwork.FilterParam(3, 3, 1, 1, True), 32)
-    network.add_batchnorm()
-    network.add_activation("relu")
+    network.add_conv_batchnorm_act(ImageNetwork.FilterParam(3, 3, 1, 1, True), 32, "relu")
     network.add_pool("MAX", ImageNetwork.FilterParam(2, 2, 2, 2, True))
-    network.add_conv(ImageNetwork.FilterParam(3, 3, 1, 1, True), 64)
-    network.add_batchnorm()
-    network.add_activation("relu")
+    network.add_conv_batchnorm_act(ImageNetwork.FilterParam(3, 3, 1, 1, True), 64, "relu")
     network.add_pool("MAX", ImageNetwork.FilterParam(2, 2, 2, 2, True))
     network.add_full_connect(1024)
     network.add_activation("relu")
@@ -32,25 +28,30 @@ def mnist_trial():
     network.add_loss("cross_entropy", name = "ce_loss")
     #network.show()
     
-    batch_size = 64
+    total_loss = network.get_total_loss(weight_decay = 1E-4)
+    batch_size = 32
     epoch_num = 100
-    lr = 1e-2
+    lr = 1e-4
     x, y = mnist.get_data("train")
     y = transform_one_hot(y, 10)
     xv, yv = mnist.get_data("val")
-    trainer = Trainer(model = network,
-                      x = x,
-                      y_dict = {"ce_loss":y})
+    yv = transform_one_hot(yv, 10)
+    optimizer = tf.train.AdamOptimizer(lr).minimize(total_loss)
     
-    for epoch in range(epoch_num):
-        batch_cnt = 0
-        for b in (range(x.shape[0] // batch_size)):
-            batch_cnt += batch_size
-            trainer.training(lr = lr,
-                             batch_size = batch_size,
-                             valid_loss_dict = None)
-            if b % 100 == 0:
-                ans_mat = trainer.predict(xv, ["answer"])[0]
-                ans = np.argmax(ans_mat, axis = 1)
-                acc = np.average(ans == yv)
-                print("[epoch={e}/{et}][batch={b}/{bt}] acc={acc}".format(e = epoch, et = epoch_num, b = batch_cnt, bt = x.shape[0], acc = acc))
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for epoch in range(epoch_num):
+            batch_cnt = 0
+            for b in (range(x.shape[0] // batch_size)):
+                batch_cnt += batch_size
+                batch_idx = np.random.choice(np.arange(x.shape[0]), batch_size, replace = True)
+                learn_feed_dict = network.create_feed_dict(input_image = x[batch_idx] / 128 - 0.5, is_training = True, label_dict = {"ce_loss": y[batch_idx]})
+                sess.run(optimizer, feed_dict = learn_feed_dict)
+                if b % 10 == 0:
+                    eval_feed_dict = network.create_feed_dict(input_image = xv / 128 - 0.5, is_training = False, label_dict = {"ce_loss": yv})
+                    ans_mat = sess.run(network.get_layer("answer"), feed_dict = eval_feed_dict)
+                    acc = np.average(np.argmax(ans_mat, axis = 1) == np.argmax(yv, axis = 1))
+                    print("[epoch={e}/{et}][batch={b}/{bt}] acc={acc}".format(e = epoch, et = epoch_num, b = batch_cnt, bt = x.shape[0], acc = acc))
+
+if "__main__" == __name__:
+    mnist_trial()
