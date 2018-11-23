22f54b369f1c0307a445da3fe2d5038eedebb3af
Yusuke Goto <isgsktyktt@gmail.com>
2018-11-23 22:04:03 +0900

    jsonが無いことを許容

--- dataset/edgeAI_detection.py
+++ dataset/edgeAI_detection.py
@@ -15,7 +15,7 @@ class EdgeAIdetection(Dataset):
         s = storage.Storage()
         self.__data_path = s.dataset_path("edgeAI_detection")
         self.__rgb_path_dict = {}
-        self.__json_path_list = None
+        self.__json_path_dict = {}
         self.__resized_h = resized_h
         self.__resized_w = resized_w
         self.__org_w = 1936
@@ -44,26 +44,20 @@ class EdgeAIdetection(Dataset):
         if data_type == "debug":
             max_num = 100
             data_type = "test"
-        if not self.__json_path_list:
-            json_dir_path = os.path.join(self.__data_path, "dtc_train_annotations")
-            assert(json_dir_path)
-            self.__json_path_list = fileio.get_file_list(tgt_dir = json_dir_path,
-                                                         tgt_ext = ".json",
-                                                         max_num = max_num)
+        if not data_type in self.__json_path_dict.keys():
+            json_dir_path = os.path.join(self.__data_path, "dtc_{}_annotations".format(data_type))
+            if os.path.exists(json_dir_path):
+                assert(os.path.isdir(json_dir_path))
+                self.__json_path_dict[data_type] = fileio.get_file_list(tgt_dir = json_dir_path,
+                                                                        tgt_ext = ".json",
+                                                                        max_num = max_num)
         if not data_type in self.__rgb_path_dict.keys():
             rgb_dir_path = os.path.join(self.__data_path, "dtc_{}_images".format(data_type))
             assert(os.path.exists(rgb_dir_path))
-            assert(len(self.__json_path_list) > 0)
-            
-            rgb_path_list = []
-            for json_path in self.__json_path_list:
-                file_name = fileio.get_file_name(json_path)[:-len(".json")] + ".jpg"
-                file_path = os.path.join(rgb_dir_path, file_name)
-                if (os.path.exists(file_path)):
-                    rgb_path_list.append(file_path)
-                else:
-                    # still not downloaded
-                    pass
+            assert(os.path.isdir(rgb_dir_path))
+            rgb_path_list = fileio.get_file_list(tgt_dir = rgb_dir_path,
+                                                 tgt_ext = ".jpg",
+                                                 max_num = max_num)
             self.__rgb_path_dict[data_type] = rgb_path_list
         return data_type
     
@@ -74,50 +68,55 @@ class EdgeAIdetection(Dataset):
     def get_vertices_data(self, data_type, tgt_words_list = None, index = None, flip = None):
         data_type = self.__update_list(data_type)
         if index is None:
-            index = np.random.randint(len(self.__json_path_list))
+            index = np.random.randint(len(self.__rgb_path_dict[data_type]))
         if index is None:
             flip = np.random.randint(2).astype(np.bool)
         
-        json_path = self.__json_path_list[index]
-
-        info = json.load(open(json_path))
-        obj_list = info["labels"]
-        
-        rect_labels = np.empty(0).astype(np.int)
-        rects = np.empty((0, 4)).astype(np.float)
-        poly_labels = np.empty(0).astype(np.int)
-        polygons = []
-        
-        for obj in obj_list:
-            label_name   = self.conv_dict[obj["category"]]
-            if not (label_name in self.label_dict.keys()):
-                print(label_name)
-                assert(0)
-            if tgt_words_list is not None:
-                if super().exists_in_words(label_name, tgt_words_list) is False:
-                    continue
-            assert(label_name in self.label_dict.keys())
-            label_value = self.label_dict[label_name]
-            if label_value != 0:
-                if "box2d" in obj.keys():
-                    box_dict = obj["box2d"]
-                    x0 = box_dict["x1"] / self.__org_w
-                    x1 = box_dict["x2"] / self.__org_w
-                    y0 = box_dict["y1"] / self.__org_h
-                    y1 = box_dict["y2"] / self.__org_h
-                    assert(x0 <= x1)
-                    assert(y0 <= y1)
-                    rects = np.append(rects, np.array([y0, x0, y1, x1]).reshape(1, 4), axis = 0)
-                    rect_labels = np.append(rect_labels, label_value)
-                elif "poly2d" in obj.keys():
-                    polygon = np.array(obj["poly2d"][0]["vertices"]).reshape(-1, 2)
-                    if polygon.shape[0] >= 3:
-                        polygon = polygon[:,::-1]   # 順番をx,yからy,xに変更
-                        polygon = polygon / np.array([self.__org_h, self.__org_w])
-                        polygons.append(polygon)
-                        poly_labels = np.append(poly_labels, label_value)
+        rect_labels = None
+        rects = None
+        poly_labels = None
+        polygons = None
+        if data_type in self.__json_path_dict.keys():
+            json_path = self.__json_path_dict[data_type][index]
     
-        rect_labels = super().convert_label_org_val(rect_labels, tgt_words_list)
+            info = json.load(open(json_path))
+            obj_list = info["labels"]
+            
+            rect_labels = np.empty(0).astype(np.int)
+            rects = np.empty((0, 4)).astype(np.float)
+            poly_labels = np.empty(0).astype(np.int)
+            polygons = []
+            
+            for obj in obj_list:
+                label_name   = self.conv_dict[obj["category"]]
+                if not (label_name in self.label_dict.keys()):
+                    print(label_name)
+                    assert(0)
+                if tgt_words_list is not None:
+                    if super().exists_in_words(label_name, tgt_words_list) is False:
+                        continue
+                assert(label_name in self.label_dict.keys())
+                label_value = self.label_dict[label_name]
+                if label_value != 0:
+                    if "box2d" in obj.keys():
+                        box_dict = obj["box2d"]
+                        x0 = box_dict["x1"] / self.__org_w
+                        x1 = box_dict["x2"] / self.__org_w
+                        y0 = box_dict["y1"] / self.__org_h
+                        y1 = box_dict["y2"] / self.__org_h
+                        assert(x0 <= x1)
+                        assert(y0 <= y1)
+                        rects = np.append(rects, np.array([y0, x0, y1, x1]).reshape(1, 4), axis = 0)
+                        rect_labels = np.append(rect_labels, label_value)
+                    elif "poly2d" in obj.keys():
+                        polygon = np.array(obj["poly2d"][0]["vertices"]).reshape(-1, 2)
+                        if polygon.shape[0] >= 3:
+                            polygon = polygon[:,::-1]   # 順番をx,yからy,xに変更
+                            polygon = polygon / np.array([self.__org_h, self.__org_w])
+                            polygons.append(polygon)
+                            poly_labels = np.append(poly_labels, label_value)
+        
+            rect_labels = super().convert_label_org_val(rect_labels, tgt_words_list)
         
         rgb_path = self.__rgb_path_dict[data_type][index]
         rgb_pil = Image.open(rgb_path)
@@ -145,36 +144,39 @@ class EdgeAIdetection(Dataset):
         dw, dh = rgb_img.size
         draw = ImageDraw.Draw(rgb_img, "RGBA")
         fontheight = draw.getfont().getsize(' ')[1]
-        for i in range(rects.shape[0]):
-            rect = rects[i]
-            y0 = (rect[0] * dh)
-            x0 = (rect[1] * dw)
-            y1 = (rect[2] * dh)
-            x1 = (rect[3] * dw)
-            draw.rectangle([x0, y0, x1, y1], outline = red)
-            for k, v in self.label_dict.items():
-                if v == rect_labels[i]:
-                    draw.text([max(0, min(dw - 1, x0)),
-                               max(0, min(dh - 1, y0 - fontheight))],
-                               k,
-                               fill = red)
-                    break
-        for i in range(len(polygons)):
-            polygon = (polygons[i] * np.array([dh , dw]))[:,::-1]
-            for k, v in self.label_dict.items():
-                if v == poly_labels[i]:
-                    draw.text([max(0, min(dw - 1, np.average(polygon[:, 0]))),
-                               max(0, min(dh - 1, np.average(polygon[:, 1])) - fontheight)],
-                               k,
-                               fill = white)
-                    if k == "lane":
-                        fill_col = green
-                    elif k == "drivable area":
-                        fill_col = blue
-                    else:
-                        assert(0)
-                    draw.polygon(polygon.flatten().tolist(), fill = fill_col)
-                    break
+        
+        if not rects is None:
+            for i in range(rects.shape[0]):
+                rect = rects[i]
+                y0 = (rect[0] * dh)
+                x0 = (rect[1] * dw)
+                y1 = (rect[2] * dh)
+                x1 = (rect[3] * dw)
+                draw.rectangle([x0, y0, x1, y1], outline = red)
+                for k, v in self.label_dict.items():
+                    if v == rect_labels[i]:
+                        draw.text([max(0, min(dw - 1, x0)),
+                                   max(0, min(dh - 1, y0 - fontheight))],
+                                   k,
+                                   fill = red)
+                        break
+        if not polygons is None:
+            for i in range(len(polygons)):
+                polygon = (polygons[i] * np.array([dh , dw]))[:,::-1]
+                for k, v in self.label_dict.items():
+                    if v == poly_labels[i]:
+                        draw.text([max(0, min(dw - 1, np.average(polygon[:, 0]))),
+                                   max(0, min(dh - 1, np.average(polygon[:, 1])) - fontheight)],
+                                   k,
+                                   fill = white)
+                        if k == "lane":
+                            fill_col = green
+                        elif k == "drivable area":
+                            fill_col = blue
+                        else:
+                            assert(0)
+                        draw.polygon(polygon.flatten().tolist(), fill = fill_col)
+                        break
         #rgb_img.show()
         return rgb_img
         
