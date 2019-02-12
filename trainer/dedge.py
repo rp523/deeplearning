d0a17c40f743969eea727978adf42366b43e5e0c
Yusuke Goto <me@example.com>
2019-02-12 23:14:09 +0900

    有効範囲も予想させる

--- trainer/dedge.py
+++ trainer/dedge.py
@@ -26,9 +26,15 @@ def dedge_net(img_h,
                     stride_y = 1
                 network.add_conv_batchnorm_act(ImageNetwork.FilterParam(3, 3, stride_y, 2, True), 32, "relu")
             else:
-                network.add_conv(ImageNetwork.FilterParam(3, 3, stride_y, 2, True), 3)
+                network.add_branching(name = "c")
+                # 有効範囲
+                network.add_full_connect(output_ch = 3, input_name = "c")
+                network.add_softmax(name = "range_output")
+                # 実際の道路エッジ
+                network.add_conv(ImageNetwork.FilterParam(3, 3, stride_y, 2, True), 3, input_name = "c")
                 network.add_batchnorm()
                 network.add_softmax(name = "output")
+
         else:
             break
 
@@ -45,13 +51,14 @@ def dedge_main():
                         img_ch = 3,
                         out_h = h_pix)
     network.add_loss(loss_type = "cross_entropy", input_name = "output", name = "edge", masking = True)
-    loss = network.get_loss_dict()["edge"]
+    network.add_loss(loss_type = "cross_entropy", input_name = "range_output", name = "range")
+    loss = network.get_loss_dict()["edge"] + network.get_loss_dict()["range"]
     opt  = tf.train.AdamOptimizer(learning_rate = lr).minimize(loss)
     bdd = BDD100k()
     train_type = "debug"
     val_type = "debug"
     log_interval_sec = 30 * 60
-    restore_path = None#"/home/isgsktyktt/workspace/deeplearning/result_20190212_074345/learned_model/modelepoch0003_batch16243"
+    restore_path = None#"/home/isgsktyktt/workspace/deeplearning/result_20190212_215637/learned_model/modelepoch0000_batch2155"
     result_dir = "result_" + datetime.now().strftime("%Y%m%d_%H%M%S")
     model_path = os.path.join(result_dir, "learned_model")
     with tf.Session() as sess:
@@ -78,6 +85,7 @@ def dedge_main():
                 if not os.path.exists(dst_dir_path):
                     os.makedirs(dst_dir_path)
                 bdd.summary_drivable_edge_data(rgb_arr, output).save(os.path.join(dst_dir_path, val_type + "_{}.png".format(i)))
+            return
 
         for epoch in range(epoch_num):
             for i in tqdm(range(bdd.get_sample_num(train_type) // batch_size)):
@@ -85,6 +93,7 @@ def dedge_main():
                 img_batch = np.zeros((batch_size, h_pix, w_pix, 3)).astype(np.float)
                 lbl_batch = np.zeros((batch_size, h_pix,     1, 3)).astype(np.float)
                 val_batch = np.zeros((batch_size, h_pix,     1, 3)).astype(np.bool)
+                range_batch = np.zeros((batch_size, 3)).astype(np.float)
                 for b in range(batch_size):
                     rgb_arr, drivable_edge = bdd.get_drivable_edge_data(data_type = train_type, h_pix = h_pix, w_pix = w_pix)
                     img_batch[b] = rgb_arr
@@ -92,6 +101,18 @@ def dedge_main():
                     valid_row = (drivable_edge[:,0] > 0.0)
                     for ch in range(3):
                         val_batch[b, :, 0, ch] = valid_row
+                    
+                    for r in range(valid_row.size):
+                        if valid_row[r] == True:
+                            range_start = r / valid_row.size
+                            break
+                    for r in range(valid_row.size):
+                        if valid_row[valid_row.size - 1 - r] == True:
+                            range_end = r / valid_row.size
+                            break
+                    range_batch[b, 0] = range_start
+                    range_batch[b, 2] = range_end
+                    range_batch[b, 1] = 1.0 - range_start - range_end
 
                     drivable_edge[drivable_edge < 0] = 0.5  # 計算後にマスクかけるので、変な値は入れちゃダメ
                     lbl_batch[b, :, 0, 0] = drivable_edge[:,0]
@@ -101,7 +122,8 @@ def dedge_main():
                 feed_dict = network.create_feed_dict(input_image = img_batch,
                                                      is_training = True,
                                                      label_dict = {"edge" : lbl_batch,
-                                                                   "edge_mask": val_batch})
+                                                                   "edge_mask": val_batch,
+                                                                   "range": range_batch})
                 assert((lbl_batch[val_batch] >= 0.0).all())
                 print(sess.run([opt, loss], feed_dict = feed_dict))
                 network.save_model(sess, saver, model_path, epoch, i, log_interval_sec)
