e56d25af62c6894ed33b9ccd539821cc5c35badf
Yusuke Goto <me@example.com>
2019-02-17 22:56:11 +0900

    changed dbug param

--- trainer/dedge.py
+++ trainer/dedge.py
@@ -28,8 +28,8 @@ def dedge_net(img_h,
             else:
                 network.add_branching(name = "c")
                 # 有効範囲
-                network.add_full_connect(output_ch = 3, input_name = "c")
-                network.add_softmax(name = "range_output")
+                #network.add_full_connect(output_ch = 3, input_name = "c")
+                #network.add_softmax(name = "range_output")
                 # 実際の道路エッジ
                 network.add_conv(ImageNetwork.FilterParam(3, 3, stride_y, 2, True), 3, input_name = "c")
                 network.add_batchnorm()
@@ -41,24 +41,28 @@ def dedge_net(img_h,
     return network
 
 def dedge_main():
-    h_pix = 128
-    w_pix = 256
+    h_pix = 256
+    w_pix = h_pix * 2
     epoch_num = 100
     batch_size = 4
-    lr = 1E-2
+    lr = 1E-7
     network = dedge_net(img_h = h_pix,
                         img_w = w_pix,
                         img_ch = 3,
                         out_h = h_pix)
-    network.add_loss(loss_type = "cross_entropy", input_name = "output", name = "edge", masking = True)
-    network.add_loss(loss_type = "cross_entropy", input_name = "range_output", name = "range")
-    loss = network.get_loss_dict()["edge"] + network.get_loss_dict()["range"]
+    #network.show_all();exit()
+    network.add_loss(loss_type = "symmetric_cross_entropy", input_name = "output", name = "edge", masking = True)
+    #network.add_loss(loss_type = "symmetric_cross_entropy", input_name = "range_output", name = "range")
+    loss = network.get_loss_dict()["edge"]# + network.get_loss_dict()["range"]
+    weight_decay = 1E-4
+    for weight in network.get_weight_list():
+        loss = loss + tf.cast(weight_decay, tf.float32) * 0.5 * tf.reduce_sum(tf.cast(weight, tf.float32) ** 2)
     opt  = tf.train.AdamOptimizer(learning_rate = lr).minimize(loss)
     bdd = BDD100k()
-    train_type = "debug"
-    val_type = "debug"
+    train_type = "train"
+    val_type = "train"
     log_interval_sec = 30 * 60
-    restore_path = None#"/home/isgsktyktt/workspace/deeplearning/result_20190212_215637/learned_model/modelepoch0000_batch2155"
+    restore_path = "/home/isgsktyktt/workspace/deeplearning/result_20190217_114146/learned_model/modelepoch0001_batch10046"
     result_dir = "result_" + datetime.now().strftime("%Y%m%d_%H%M%S")
     model_path = os.path.join(result_dir, "learned_model")
     with tf.Session() as sess:
@@ -78,9 +82,14 @@ def dedge_main():
                 rgb_arr, drivable_edge = bdd.get_drivable_edge_data(data_type = val_type, index = i, h_pix = h_pix, w_pix = w_pix)
                 feed_dict = network.create_feed_dict(input_image = np.reshape(rgb_arr, [-1] + list(rgb_arr.shape)),
                                                     is_training = False)
-                output = sess.run(network.get_layer("output"), feed_dict = feed_dict)
+                output = sess.run((network.get_layer("output")), feed_dict = feed_dict)
                 output = output.reshape(output.shape[1], 3)
                 output = np.delete(output, 1, axis = 1)
+                #range_output = range_output.flatten()
+                #range_output[1] = (1.0 - range_output[2])
+                #range_output = range_output[:2]
+                #print(range_output)
+                #range_output = range_output * rgb_arr.shape[1]
                 dst_dir_path = os.path.join(result_dir, "val")
                 if not os.path.exists(dst_dir_path):
                     os.makedirs(dst_dir_path)
@@ -102,6 +111,8 @@ def dedge_main():
                     for ch in range(3):
                         val_batch[b, :, 0, ch] = valid_row
                     
+                    range_start = 1.0
+                    range_end = 0.0
                     for r in range(valid_row.size):
                         if valid_row[r] == True:
                             range_start = r / valid_row.size
@@ -122,10 +133,10 @@ def dedge_main():
                 feed_dict = network.create_feed_dict(input_image = img_batch,
                                                      is_training = True,
                                                      label_dict = {"edge" : lbl_batch,
-                                                                   "edge_mask": val_batch,
-                                                                   "range": range_batch})
+                                                                   "edge_mask": val_batch})#,
+                                                                   #"range": range_batch})
                 assert((lbl_batch[val_batch] >= 0.0).all())
-                print(sess.run([opt, loss], feed_dict = feed_dict))
+                print(sess.run([opt, loss, [network.get_loss_dict()["edge"]]], feed_dict = feed_dict))
                 network.save_model(sess, saver, model_path, epoch, i, log_interval_sec)
         network.save_model(sess, saver, model_path)
     from PIL import Image
